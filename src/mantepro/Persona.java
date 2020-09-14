5b8475dc26ff7c0956728f558b1dfebc9e3a546d
DanM10 <daniel.garrido.w8@hotmail.com>
2020-09-13 22:55:22 -0500

    ajuste bici/persona
    se eliminaron datos innecesarios

--- src/mantepro/Persona.java
+++ src/mantepro/Persona.java
@@ -5,6 +5,8 @@
  */
 package mantepro;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Daniel
@@ -14,9 +16,8 @@ public abstract class Persona {
     private String nombre;
     private int edad;
     private String formaComuni;
-    private Bici bici;
-    private Puertas puerta ;
-    private Ventana[] ventanas=new Ventana[2];
+    
+    ArrayList<TareasOrganizar> tareaslist = new ArrayList<TareasOrganizar>();
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
